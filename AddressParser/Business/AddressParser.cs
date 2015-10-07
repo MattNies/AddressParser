cb4caab803b81d557a0d2868e3f6d6471e96cf65
unknown <mnies@LC38508.ad.lexcosc.gov>
2015-10-07 16:01:29 -0400

    Initial project

---
+++ AddressParser/Business/AddressParser.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace AnimalTrap.Business
+{
+    public enum State
+    {
+        AL, AK, AZ, AR, CF, CL, CT, DL, DC, FL, GA, HI, ID, IL, IN, IA, KA, KY, LA, ME, MD, MA, MI, MN, MS, MO, MT, NE, NV, NH,
+        NJ, NM, NY, NC, ND, OH, OK, OR, PA, RI, SC, SD, TN, TX, UT, VT, VA, WA, WV, WI, WY
+    }
+
+    public class AddressParser
+    {
+        #region fields
+
+        private static char[] Address2Separator = new char[] { ' ', ',' };
+        private const string ZipCodeRegexUS = @"^\d{5}(?:[-\s]\d{4})?$";
+
+        #endregion
+
+        #region methods
+        
+        public static void ParseAddressLine1(string address)
+        {
+
+        }
+
+        public static string[] ParseAddressLine2(string address)
+        {
+            address = address.Replace(',', ' ');
+            List<string> parsedAddress = new List<string>();
+            string[] returnAddress = new string[3];
+
+            foreach (string s in Regex.Split(address, @"\s{1,}"))
+            {
+                parsedAddress.Add(s);
+            }
+
+            //
+            foreach(string s in parsedAddress)
+            {
+                if (Regex.IsMatch(s, ZipCodeRegexUS))
+                {
+                    returnAddress[2] = s;      
+                }
+            }
+            parsedAddress.Remove(returnAddress[2]);
+
+            //
+            foreach(string s in parsedAddress)
+            {
+                if (Enum.IsDefined(typeof(State), s))
+                {
+                    returnAddress[1] = s;    
+                }
+            }
+            parsedAddress.Remove(returnAddress[1]);
+
+            //
+            returnAddress[0] = string.Join(" ", parsedAddress);
+
+            return returnAddress;
+        }
+
+        public static bool ValidateStreetTypes(string streetType)
+        {
+            string[] streetTypes = new string[]
+            {
+                "ALLEY", "ALY",
+                "AVENUE", "AVE",
+                "BOULEVARD", "BLVD",
+                "BEND", "BND",
+                "CIRCLE", "CIR",
+                "CRESCENT", "CRES",
+                "CROSSING", "CRNG",
+                "COURT", "CT",
+                "DRIVE", "DR",
+                "END", 
+                "ESTATES", "ESTS",
+                "EXPRESSWAY", "EXPR",
+                "EXTENSION", "EXT",
+                "HIGHWAY", "HWY",
+                "LANE", "LN",
+                "LOOP",
+                "PASS",
+                "PATH",
+                "PARKWAY", "PKWY",
+                "PLACE", "PL",
+                "POINT", "PT",
+                "POINTE", "PTE",
+                "RAMP",
+                "ROAD", "RD",
+                "ROW",
+                "ROUTE", "RTE",
+                "RUN",
+                "SQUARE", "SQ",
+                "STREET", "ST",
+                "TERRACE", "TER",
+                "TRACE", "TRC",
+                "TRAIL", "TRL",
+                "WAY"
+            };
+
+            bool check = streetTypes.Contains(streetType.ToUpper());
+
+            return check;
+        }
+
+
+        public static bool ValidateState(string state)
+        {
+            return true;
+        }
+
+        public static bool ValidateZipCode(string zipCode)
+        {
+            Regex reg = new Regex(ZipCodeRegexUS);
+
+            if (reg.IsMatch(zipCode))
+                return true;
+            else
+                return false;
+        }
+
+
+
+        #endregion
+
+    }
+}
---
+++ AddressParser/Objects/Address.cs
@@ -0,0 +1,101 @@
+﻿using AnimalTrap.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AnimalTrap.Objects
+{
+
+    public class Address
+    {
+
+        #region fields
+
+        #endregion
+
+        #region properties
+
+        public string Address1 { get; private set; } // street address, apt/lot information
+        public string Address2 { get; private set; } // city, state zip
+
+        public string StreetNumber { get; private set; }
+        public string StreetNumberSuffix { get; private set; }
+        public string PrefixDirectional { get; private set; }
+        public string StreetName { get; private set; }
+        public string StreetType { get; private set; }
+        public string SuffixDirectional { get; private set; }
+        public string UnitType { get; private set; }
+        public string Unit { get; private set; }
+
+        public string City { get; private set; }
+        public string DefaultCity { get; private set; }
+        public string State { get; private set; }
+        public string ZipCode { get; private set; }
+
+        #endregion
+
+        #region constructors
+
+        public Address(string address1, string address2)
+        {
+            Address1 = address1;
+            Address2 = address2;
+        }
+
+        #endregion
+
+        #region methods
+
+        private void SetAdress1(string address)
+        {
+            string[] parsedAddress;
+
+
+        }
+
+        private void ParseAdress2(string address)
+        {
+            string[] parsedAddress = AddressParser.ParseAddressLine2(address);
+
+            City = parsedAddress[0];
+            State = parsedAddress[1];
+            ZipCode = parsedAddress[2];
+        }
+
+        private string GetDefaultCity()
+        {
+            if (this.ZipCode.Equals("29006")) 
+                return "Batesburg";
+            if (this.ZipCode.Equals("29033")) 
+                return "Cayce";
+            if (this.ZipCode.Equals("29036")) 
+                return "Chapin";
+            if (this.ZipCode.Equals("29054"))
+                return "Gaston";
+            if (this.ZipCode.Equals("29063"))
+                return "Irmo";
+            if (this.ZipCode.Equals("29070"))
+                return "Leesville";
+            if (this.ZipCode.Equals("29072") || this.ZipCode.Equals("29073"))
+                return "Lexington";
+            if (this.ZipCode.Equals("29075"))
+                return "Little Mountain";
+            if (this.ZipCode.Equals("29112"))
+                return "North";
+            if (this.ZipCode.Equals("29123"))
+                return "Pelion";
+            if (this.ZipCode.Equals("29160"))
+                return "Swansea";
+            if (this.ZipCode.Equals("29169") || this.ZipCode.Equals("29170") || this.ZipCode.Equals("29172"))
+                return "West Columbia";
+            if (this.ZipCode.Equals("29210") || this.ZipCode.Equals("29212"))
+                return "Columbia";
+
+            return String.Empty;
+        }
+
+
+        #endregion
+    }
+}

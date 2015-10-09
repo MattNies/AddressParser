2886f43d81c39576fbc47f992c229ffdf888cbe2
MattNies <mattcnies@gmail.com>
2015-10-09 09:05:20 -0400

    project file updates

--- AddressParser/Program.cs
+++ AddressParser/Program.cs
@@ -13,8 +13,29 @@ namespace AddressParser
         static void Main(string[] args)
         {
 
+            Console.WriteLine(new Address("752 HIGHWAY 378 W", "Lexington, SC 29072", "").GetAddress());
+            Console.WriteLine(new Address("212B S Lake Dr W", "Lexington, SC 29072", "Suite 302").GetAddress());
             Console.WriteLine(new Address("212B S Lake Dr West", "Lexington, SC 29072", "Suite 302").GetAddress());
-            //string[] parsedl1 = ParseAddress.ParseAddressLine1(l1);
+            Console.WriteLine(new Address("121 Northpoint Dr", "Lexington, SC 29072", "Apt 210").GetAddress());
+            Console.WriteLine(new Address("104 BRIDLE CT", "Lexington, SC 29072", String.Empty).GetAddress());
+            Console.WriteLine(new Address("143 Hunters Ridge Dr", "Lexington, SC 29072", String.Empty).GetAddress());
+            Console.WriteLine(new Address("4590 Augusta Rd", "Lexington, SC 29073", String.Empty).GetAddress());
+            Console.WriteLine(new Address("131 New Orangeburg Rd", "Lexington, SC 29073", "").GetAddress());
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+                
+                
+                
+                //string[] parsedl1 = ParseAddress.ParseAddressLine1(l1);
 
             //foreach (string s in parsedl1)
             //{
